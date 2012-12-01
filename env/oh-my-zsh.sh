ea22ad8e27038268bb39a7e836506299d24e3c59
bee <bee@cell.hive>
2012-11-30 21:35:00 -0800

    fixed bugs.

--- env/oh-my-zsh.sh
+++ env/oh-my-zsh.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install -O - | sh
+wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
