61a9e7a8959b9abc3bbacf5139ec9602266cdb3c
bee <bee@cell.hive>
2012-12-01 00:55:10 -0800

    oops.

--- env/oh-my-zsh.sh
+++ env/oh-my-zsh.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-wget --no-check-certificate https://github.com/ahdinosaur/skel/raw/master/install -O - | sh
+wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install -O - | sh
