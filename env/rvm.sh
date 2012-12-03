6268b11d062d55b62b9645207caa1fdefdbb8566
Michael Williams <michael@dinosaur.io>
2012-12-02 21:31:41 -0800

    moved run into install.

--- env/rvm.sh
+++ env/rvm.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+echo "installing rvm"
 wget --no-check-certificate https://get.rvm.io -O - | bash -s stable --ruby
-source ~/.rvm/scripts/rvm
+
+echo "deleting zprofile and zshrc made by rvm install."
 rm ~/.zprofile
 rm ~/.zshrc
